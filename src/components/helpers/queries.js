e718ca87271729075fc55f634fa440160521ec4e
Luciano Angeleri <lcnang@gmail.com>
2023-06-13 16:52:53 -0300

    Agregamos validaciones del login, en el email y passoword

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,3 +1,6 @@
 //llamar a una variable de entorno
 const URL_usuario = inport.meta.env.VITE_API_USUARIO
 const URL_producto = inport.meta.env.VITE_API_PRODUCTO
+
+console.log(URL_usuario)
+console.log(URL_producto)
